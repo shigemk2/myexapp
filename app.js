be87dc16900c7ac7cd0c675b69973e60ce5744e5
shigemk2 <i.am.shige@gmail.com>
2014-06-16 22:10:05 +0900

    P148

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+index_post = require('./routes/index_post');
 
 var app = express();
 
@@ -30,6 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.post('/', index_post.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/index_post.js
@@ -0,0 +1,6 @@
+exports.index = function(req, res) {
+    res.render('index', {
+        title: 'Express',
+        msg: 'こんにちは ' + req.body.text1 + 'さん'
+    });
+};
