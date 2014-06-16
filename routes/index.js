7c221941c6b093e2be65289ddd0a16023522271d
shigemk2 <i.am.shige@gmail.com>
2014-06-16 22:22:34 +0900

    P149

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,18 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', {
-      title: 'Express',
-      msg: 'こんにちは'
-  });
+    var id = req.query.id;
+    var name = req.query.name;
+    var msg = 'こんにちは';
+
+    if (id != undefined) {
+        msg += 'ID=' + id + '番';
+    };
+    if (name != undefined) {
+        msg += name + 'さん';
+    };
+    res.render('index', {
+        title: 'Express',
+        msg: msg
+    });
 };
