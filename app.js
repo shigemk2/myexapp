691ae54dd664385e9946f2ba99d3cf847f667f20
shigemk2 <i.am.shige@gmail.com>
2014-06-17 23:15:16 +0900

    P157

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser('secret','mycom_sercred_key'));
+app.use(express.session({key:'session_id'}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,13 @@
  */
 
 exports.index = function(req, res){
-    var id = req.params.id;
-    var name = req.params.name;
-    var msg = 'こんにちは';
+    var msg = '';
+    if (req.session.login != true) {
+        msg = 'ログインしてください';
+    } else {
+        msg = 'ID:' + req.session.name + 'でログインしています。';
+    };
 
-    if (id != undefined) {
-        msg += 'ID=' + id + '番';
-    };
-    if (name != undefined) {
-        msg += name + 'さん';
-    };
     res.render('index', {
         title: 'Express',
         msg: msg
